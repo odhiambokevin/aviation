266e41929d29f86ed02fa3609d5b262c8923a9fb
odhiambokevin <kevinabrahamsodhiambo@gmail.com>
2022-11-11 14:52:05 +0300

    refactor(root route)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
-import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route, Outlet } from 'react-router-dom';
 import Home from "./pages/Home";
 import Blog from './pages/Blog';
 import Header from './components/Header';
 import NotFound from './pages/NotFound';
+import Footer from './components/Footer';
+
+export const AppLayout = ()=>{
+  return(
+    <>
+    <Header />
+    <Outlet />
+    <Footer />
+    </>
+  )
+};
 
 const router = createBrowserRouter(createRoutesFromElements(
-  <>
-  <Route path='/' element={<Home />} errorElement={<NotFound />} />
-  <Route path='/blogs' element={<Blog />} errorElement={<NotFound />} />
-  </>
+  <Route element={<AppLayout />}>
+    <Route path='/' element={<Home />} errorElement={<NotFound />} />
+    <Route path='/blogs' element={<Blog />} errorElement={<NotFound />} />
+  </Route>
   ));
 
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,19 +4,16 @@ import Skills from "../components/Skills";
 import Contact from "../components/Contact";
 import Portfolio from "../components/Portfolio";
 import Services from "../components/Services";
-import Footer from '../components/Footer';
-import Header from '../components/Header';
+
 
 function Home() {
     return (
         <>
-        <Header />
         <Homeheader />
         <Portfolio />
         <Skills />
         <Services />
         <Contact />
-        <Footer />
         <Link to="home" spy={true} smooth={true} offset={-10} duration={1500} className="scrollup"> <i className="fa fa-chevron-up"> </i> </Link>
         </>
      );
