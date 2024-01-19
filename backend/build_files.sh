72ed32e4e20cf2d6d3120adeeba4aa70bd741328
odhiambokevin <kevinabrahamsodhiambo@gmail.com>
2024-01-19 18:57:32 +0300

    chore: vercel config refactor

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,5 +1,2 @@
-sudo apt-get update
-sudo apt-get install -y libpq-dev gdal-bin libgdal-dev libgeos-dev
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
-python3.9 manage.py createsuperuser --no-input
